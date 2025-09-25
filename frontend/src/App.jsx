460db0500569b3669af5c969d794516052000756
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-09-25 19:50:40 +0700

    setting: routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  AddressForm,
+  Blog,
+  Cart,
+  Contact,
+  Home,
+  Menu,
+  MyOrders,
+} from "./pages";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 const App = () => {
-  return <main>App</main>;
+  return (
+    <main className=" overflow-x-hidden text-textColor">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/address-form" element={<AddressForm />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+      </Routes>
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as Blog } from "./Blog";
+export { default as AddressForm } from "./AddressForm";
+export { default as Cart } from "./Cart";
+export { default as Contact } from "./Contact";
+export { default as Menu } from "./Menu";
+export { default as MyOrders } from "./MyOrders";
