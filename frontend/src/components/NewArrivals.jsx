d297073ead9b51b3955f236d122c3ccfd7d34bcf
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-09-27 15:42:48 +0700

    Feature: Adding NewArrivals

---
+++ frontend/src/components/NewArrivals.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Title from "./ui/Title";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
+import { Autoplay } from "swiper/modules";
+import { dummyProducts } from "../assets/data";
+import Item from "./Item";
+
+const NewArrivals = () => {
+  const [NewArrivals, setNewArrivals] = useState([]);
+
+  useEffect(() => {
+    const data = dummyProducts.filter((item) => item.inStock).slice(0, 10);
+    setNewArrivals(data);
+  }, [dummyProducts]);
+  return (
+    <section className="max-padd-container py-22 xl:py-28 bg-white">
+      <Title title1={"New"} title2={"Arrivals"} titleStyles={"pb-10"} />
+      <Swiper
+        spaceBetween={30}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        modules={[Autoplay]}
+        breakpoints={{
+          500: { slidesPerView: 2 },
+          700: { slidesPerView: 3 },
+          1022: { slidesPerView: 4 },
+          1350: { slidesPerView: 5 },
+        }}
+        className="min-h-[399px]"
+      >
+        {NewArrivals.map((product) => (
+          <SwiperSlide key={product._id}>
+            <Item product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default NewArrivals;
