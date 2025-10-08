c16f73a9051fefae4493e2ec44b079066927f576
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-10-08 19:15:20 +0700

    change: fix dataType

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     image: { type: String, required: true },
     role: { type: String, enum: ["user", "owner"], default: "user" },
-    cartData: { type: String, default: {} },
+    cartData: { type: Object, default: {} },
   },
   { timestamps: true, minimize: false }
 );
