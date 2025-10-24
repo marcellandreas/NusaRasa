bb6f90958a6954ace3e964ee465cb3f0fe16971a
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-10-24 19:56:36 +0700

    fix: improve order display logic by ensuring unique keys for items; enhance readability and maintainability of order summary

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -43,119 +43,124 @@ const Dashboard = () => {
       </div>
       {/* All orders/sales */}
       <div className=" bg-primary mt-4 ">
-        {dashboardData.orders.map((order) => (
-          <div key={order._id} className=" bg-white p-2 mt-3 rounded-2xl">
-            <div className="flex flex-wrap gap-8 gap-y-3 mb-3">
-              {/* orders items */}
-              {order.items.map((item, idx) => (
-                <div key={idx} className=" text-gray-700 w-full lg:w-1/3">
-                  <div className="flex flex-[2] gap-x-3">
-                    <div className=" flexCenter bg-primary rounded-xl">
-                      <img
-                        src={item.product.images[0]}
-                        className="max-h-20 max-w-20 object-contain"
-                        alt=""
-                      />
-                    </div>
-                    <div className=" block w-full">
-                      <h5 className=" uppercase line-clamp-1">
-                        {item.product.title}
-                      </h5>
-                      <div className="flex flex-wrap gap-3 max-sm:gap-y-1 mt-1">
-                        <div className="flex items-center gap-x-2">
-                          <h5 className="text-sm font-medium">Price:</h5>
-                          <p>
-                            {currency}
-                            {item.product.price["H"]}
-                          </p>
-                        </div>
-                        <div className="flex items-center gap-x-2">
-                          <h5 className="text-sm font-medium">Quantity:</h5>
-                          <p>{item.quantity}</p>
-                        </div>
-                        <div className="flex items-center gap-x-2">
-                          <h5 className="text-sm font-medium">Size:</h5>
-                          <p>{item.size}</p>
+        {dashboardData.orders.map((order) => {
+          return (
+            <div key={order._id} className=" bg-white p-2 mt-3 rounded-2xl">
+              <div className="flex flex-wrap gap-8 gap-y-3 mb-3">
+                {/* orders items */}
+                {order.items.map((item, idx) => (
+                  <div
+                    key={item._id || idx}
+                    className=" text-gray-700 w-full lg:w-1/3"
+                  >
+                    <div className="flex flex-[2] gap-x-3">
+                      <div className=" flexCenter bg-primary rounded-xl">
+                        <img
+                          src={item.product.images[0]}
+                          className="max-h-20 max-w-20 object-contain"
+                          alt=""
+                        />
+                      </div>
+                      <div className=" block w-full">
+                        <h5 className=" uppercase line-clamp-1">
+                          {item.product.title}
+                        </h5>
+                        <div className="flex flex-wrap gap-3 max-sm:gap-y-1 mt-1">
+                          <div className="flex items-center gap-x-2">
+                            <h5 className="text-sm font-medium">Price:</h5>
+                            <p>
+                              {currency}
+                              {item.product.price["H"]}
+                            </p>
+                          </div>
+                          <div className="flex items-center gap-x-2">
+                            <h5 className="text-sm font-medium">Quantity:</h5>
+                            <p>{item.quantity}</p>
+                          </div>
+                          <div className="flex items-center gap-x-2">
+                            <h5 className="text-sm font-medium">Size:</h5>
+                            <p>{item.size}</p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
+                ))}
+              </div>
+              {/* orders summary */}
+              <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4 border-t border-gray-300 pt-3">
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center gap-x-2">
+                    <h5 className="text-sm font-medium">OrderId:</h5>
+                    <p className=" text-gray-400 text-sm">{order._id}</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Customer</h5>
+                      <p className=" text-gray-400 text-sm">
+                        {order.address.firstName}
+                        {order.address.lastName}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Phone:</h5>
+                      <p className=" text-gray-400 text-sm">
+                        {order.address.phone}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-x-2">
+                    <h5 className="text-sm font-medium">Address:</h5>
+                    <p className=" text-gray-400 text-sm">
+                      {order.address.street}, {order.address.city},
+                      {order.address.zipcode} {order.address.state},{" "}
+                      {order.address.country}
+                    </p>
+                  </div>
+                  <div className="flex gap-4">
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Payment Status:</h5>
+                      <p className=" text-gray-400 text-sm">
+                        {order.isPaid ? "Done" : "Pending"}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Method:</h5>
+                      <p className=" text-gray-400 text-sm">
+                        {order.paymentMethod}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex gap-4">
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Date:</h5>
+                      <p className=" text-gray-400 text-sm">
+                        {new Date(order.createdAt).toDateString()}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                      <h5 className="text-sm font-medium">Amount:</h5>
+                      <p className=" text-gray-400 text-sm">{order.amount}</p>
+                    </div>
+                  </div>
                 </div>
-              ))}
-            </div>
-            {/* orders summary */}
-            <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center gap-4 border-t border-gray-300 pt-3">
-              <div className="flex flex-col gap-2">
                 <div className="flex items-center gap-x-2">
-                  <h5 className="text-sm font-medium">OrderId:</h5>
-                  <p className=" text-gray-400 text-sm">{order._id}</p>
-                </div>
-                <div className="flex gap-4">
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Customer</h5>
-                    <p className=" text-gray-400 text-sm">
-                      {order.address.firstName}
-                      {order.address.lastName}
-                    </p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Phone:</h5>
-                    <p className=" text-gray-400 text-sm">
-                      {order.address.phone}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-x-2">
-                  <h5 className="text-sm font-medium">Address:</h5>
-                  <p className=" text-gray-400 text-sm">
-                    {order.address.street}, {order.address.city},
-                    {order.address.zipcode} {order.address.state},{" "}
-                    {order.address.country}
-                  </p>
-                </div>
-                <div className="flex gap-4">
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Payment Status:</h5>
-                    <p className=" text-gray-400 text-sm">
-                      {order.isPaid ? "Done" : "Pending"}
-                    </p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Method:</h5>
-                    <p className=" text-gray-400 text-sm">
-                      {order.paymentMethod}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex gap-4">
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Date:</h5>
-                    <p className=" text-gray-400 text-sm">
-                      {new Date(order.createdAt).toDateString()}
-                    </p>
-                  </div>
-                  <div className="flex items-center gap-x-2">
-                    <h5 className="text-sm font-medium">Amount:</h5>
-                    <p className=" text-gray-400 text-sm">{order.amount}</p>
-                  </div>
+                  <h5 className="text-sm font-medium">Status:</h5>
+                  <select
+                    defaultValue={order.status}
+                    className=" text-sm font-semibold p-1 ring-1 ring-slate-900/5 rounded max-w-36 bg-primary"
+                  >
+                    <option value="Order Placed">Order Placed</option>
+                    <option value="Packing">Packing</option>
+                    <option value="Shipping">Shipping</option>
+                    <option value="Out for delivery">Out for delivery</option>
+                    <option value="Delivered">Delivered</option>
+                  </select>
                 </div>
               </div>
-              <div className="flex items-center gap-x-2">
-                <h5 className="text-sm font-medium">Status:</h5>
-                <select
-                  value={order.status}
-                  className=" text-sm font-semibold p-1 ring-1 ring-slate-900/5 rounded max-w-36 bg-primary"
-                >
-                  <option value="Order Placed">Order Placed</option>
-                  <option value="Packing">Packing</option>
-                  <option value="Shipping">Shipping</option>
-                  <option value="Out for delivery">Out for delivery</option>
-                  <option value="Delivered">Delivered</option>
-                </select>
-              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
